1d309618a367a8a73f41eb3d67f72b4801eb37b5
le4f <le4f@qq.com>
2014-05-06 14:12:45 +0800

    GSM Sniffer
    GSM 短信嗅探分析套件

---
+++ gsmhack/start_osmbb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/osmocom-bb/src/host/osmocon/osmocon -m c123xor -p /dev/ttyUSB0 ~/osmocom-bb/src/target/firmware/board/compal_e88/layer1.compalram.bin
---
+++ readme.md
@@ -0,0 +1,10 @@
+GSM SMS Sniffer
+===========
+
+C118 短信嗅探集成利用套件
+
+附演示DEMO视频
+
+代码参考:https://github.com/5a43/gsm.git
+
+Links:[Le4F.net](http://le4f.net)
